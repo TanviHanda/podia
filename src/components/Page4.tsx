f7ec3292340e98f978defd850bbb8a6779f7eb29
TanviHanda <tanvisona2012@gmail.com>
2025-10-26 15:57:24 +0530

    fix : page 4 elemements size and gap for mobile screen

--- src/components/Page4.tsx
+++ src/components/Page4.tsx
@@ -7,25 +7,25 @@ import { BsCheck } from "react-icons/bs";
 
 const Page4: React.FC = () => {
   return (
-    <section className="p-8 min-h-screen">
+    <section className="p-4 min-h-screen lg:p-8">
       <div>
-        <ul className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
           {features.map((item: Feature) => (
             
             <li 
-              className="bg-white p-6  border flex items-start gap-4"
+              className="bg-white p-4  border flex items-start gap-4"
             >
               
               {/* 3. Add the icon wrapper */}
               <div className="flex-shrink-0 bg-gray-100 p-2 rounded-md">
-                <BsCheck className="w-6 h-6 text-black" />
+                <BsCheck className="lg:w-6 lg:h-6 text-black w-5 h-5" />
               </div>
 
               {/* 4. This div now holds all your text content */}
               <div>
-                <h3 className="text-3xl font-bold mb-2">{item.title}</h3>
+                <h3 className="lg:text-3xl font-bold mb-2 text-2xl">{item.title}</h3>
                 <div>
-                  <p className="text-2xl font-semibold">{item.description}</p>
+                  <p className="lg:text-2xl font-semibold text-xl">{item.description}</p>
                 </div>
               </div>
 
